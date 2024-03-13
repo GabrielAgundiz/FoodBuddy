7c3ec2a03debc67eba061999765b0143a1ee9aa8
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2024-03-13 01:03:15 -0600

    Favoritos

--- foodbuddy/lib/screens/wishlist.dart
+++ foodbuddy/lib/screens/wishlist.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:foodbuddy/models/food.dart';
+import 'package:foodbuddy/widgets/food_card.dart';
 
 class SavesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Favoritos', style: TextStyle(fontWeight: FontWeight.bold),),
+        title: const Text(
+          'Favoritos',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
-      body: const Center(
+      body: SafeArea(
         child: SingleChildScrollView(
-          child: Text(
-            'Bienvenido a la página de Favoritos',
-            style: TextStyle(fontSize: 24),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 14,
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                  children: foods
+                      .where((food) => food.isLiked)
+                      .map((food) => Platillo(
+                            food: food,
+                          ))
+                      .toList(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
