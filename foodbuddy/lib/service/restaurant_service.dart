4b03b03bf1849c60e8679899f64f0efdf52246a1
Americo <agzz2003@gmail.com>
2024-05-25 14:38:26 -0600

    Servicio de restaurantes

--- foodbuddy/lib/service/restaurant_service.dart
+++ foodbuddy/lib/service/restaurant_service.dart
@@ -1,27 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:foodbuddy/models/food.dart';
+import 'package:foodbuddy/models/restaurants.dart';
 
 class RestaurantService {
   final restaurantRef =
       FirebaseFirestore.instance.collection('restaurant').withConverter(
             fromFirestore: (snapshot, _) =>
-                Food.fromJson(snapshot.id, snapshot.data()!),
+                Restaurants.fromJson(snapshot.id, snapshot.data()!),
             toFirestore: (food, _) => food.toJson(),
           );
 
-  Future<List<Food>> getFoods() async {
-    //nos devuelve un future que devuelve una lista de libros
-    //apartir de mi referencia obtengo los datos y regresa la respuesta (query que contiene book)
+  Future<List<Restaurants>> getRestaurants() async {
     var result = await restaurantRef.get().then((value) => value);
-    //obtiene los tres ultimos
-    List<Food> foods = []; //lista vacia
+    List<Restaurants> restaurants = []; //lista vacia
 
     for (var doc in result.docs) {
-      //se itera para cada uno de los docs(tablas) que vienen en mi resultado
-      //se agrega el libro que viene en cada doc en su data a mi lista de libros books
-      foods.add(doc.data());
+      restaurants.add(doc.data());
     }
     //refresa un future value de la lista de libros obtenida
-    return Future.value(foods);
+    return Future.value(restaurants);
   }
 }
